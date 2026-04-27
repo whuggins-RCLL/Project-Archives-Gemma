b4df17e4ac9b3f66172e6777e0c4b00149101749
Will Huggins <whuggins@stanford.edu>
2026-04-27 12:29:18 -0700

    Preload branding settings before app mount

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { preloadBrandingSettings } from './hooks/useBranding';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+async function bootstrap() {
+  await preloadBrandingSettings();
+
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+}
+
+void bootstrap();
